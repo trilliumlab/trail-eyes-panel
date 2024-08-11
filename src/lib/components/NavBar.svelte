97413e7ffab321f3680087c708ed015abd3e856e
Elliot Nash <elliotnash@gmail.com>
2024-08-10 23:00:17 -0700

    Use snippets in navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,30 +1,34 @@
-<script>
+<script lang="ts">
   import Package2 from 'lucide-svelte/icons/package-2';
   import UserMenu from '$lib/components/UserMenu.svelte';
   import { Button } from '$lib/components/ui/button/index';
   import Menu from 'lucide-svelte/icons/menu';
   import * as Sheet from '$lib/components/ui/sheet/index';
+  import { page } from '$app/stores';
 </script>
+
+{#snippet link(path: string, name: string)}
+  <a
+    href={path}
+    class="{$page.url.pathname === path
+      ? 'text-foreground'
+      : 'text-muted-foreground'} transition-colors hover:text-foreground"
+  >
+    {name}
+  </a>
+{/snippet}
 
 <header class="bg-background sticky top-0 flex h-16 items-center gap-4 border-b px-4 md:px-6">
   <nav
     class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
   >
-    <a href="##" class="flex items-center gap-2 text-lg font-semibold md:text-base">
+    <a href="/" class="flex items-center gap-2 text-lg font-semibold md:text-base">
       <img src="/favicon.png" alt="TrailEyes Panel" class="h-8 w-8" />
       <span class="sr-only">TrailEyes Panel</span>
     </a>
-    <a href="##" class="text-foreground hover:text-foreground transition-colors"> Dashboard </a>
-    <a href="##" class="text-muted-foreground hover:text-foreground transition-colors"> Orders </a>
-    <a href="##" class="text-muted-foreground hover:text-foreground transition-colors">
-      Products
-    </a>
-    <a href="##" class="text-muted-foreground hover:text-foreground transition-colors">
-      Customers
-    </a>
-    <a href="##" class="text-muted-foreground hover:text-foreground transition-colors">
-      Analytics
-    </a>
+    {@render link('/', 'Dashboard')}
+    {@render link('/confirmed', 'Confirmed Reports')}
+    {@render link('/unconfirmed', 'Unconfirmed Reports')}
   </nav>
   <Sheet.Root>
     <Sheet.Trigger asChild let:builder>
