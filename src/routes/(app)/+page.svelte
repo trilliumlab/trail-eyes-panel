b42e73ffb21f4c062420f8aca74ececda4449fc0
Elliot Nash <elliotnash@gmail.com>
2024-08-10 22:37:28 -0700

    Oops

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -16,10 +16,7 @@
   <NavBar />
   <main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
     <div class="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-      <Card.Root
-        data-x-chunk-name="dashboard-01-chunk-0"
-        data-x-chunk-description="A card showing the total revenue in USD and the percentage difference from last month."
-      >
+      <Card.Root>
         <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
           <Card.Title class="text-sm font-medium">Total Revenue</Card.Title>
           <DollarSign class="text-muted-foreground h-4 w-4" />
@@ -42,10 +39,7 @@
           <p class="text-muted-foreground text-xs">+180.1% from last month</p>
         </Card.Content>
       </Card.Root>
-      <Card.Root
-        data-x-chunk-name="dashboard-01-chunk-2"
-        data-x-chunk-description="A card showing the total sales and the percentage difference from last month."
-      >
+      <Card.Root>
         <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
           <Card.Title class="text-sm font-medium">Sales</Card.Title>
           <CreditCard class="text-muted-foreground h-4 w-4" />
@@ -55,10 +49,7 @@
           <p class="text-muted-foreground text-xs">+19% from last month</p>
         </Card.Content>
       </Card.Root>
-      <Card.Root
-        data-x-chunk-name="dashboard-01-chunk-3"
-        data-x-chunk-description="A card showing the total active users and the percentage difference from last hour."
-      >
+      <Card.Root>
         <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
           <Card.Title class="text-sm font-medium">Active Now</Card.Title>
           <Activity class="text-muted-foreground h-4 w-4" />
@@ -70,11 +61,7 @@
       </Card.Root>
     </div>
     <div class="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-      <Card.Root
-        class="xl:col-span-2"
-        data-x-chunk-name="dashboard-01-chunk-4"
-        data-x-chunk-description="A card showing a table of recent transactions with a link to view all transactions."
-      >
+      <Card.Root class="xl:col-span-2">
         <Card.Header class="flex flex-row items-center">
           <div class="grid gap-2">
             <Card.Title>Transactions</Card.Title>
@@ -173,10 +160,7 @@
           </Table.Root>
         </Card.Content>
       </Card.Root>
-      <Card.Root
-        data-x-chunk-name="dashboard-01-chunk-5"
-        data-x-chunk-description="A card showing a list of recent sales with customer names and email addresses."
-      >
+      <Card.Root>
         <Card.Header>
           <Card.Title>Recent Sales</Card.Title>
         </Card.Header>
